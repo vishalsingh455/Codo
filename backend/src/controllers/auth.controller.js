2e60928774fe93445c10f9a3bde9d06c121dd377
vishalsingh455 <thevishalsingh79@gmail.com>
2026-01-03 23:30:17 +0530

    Revert "frontend improved"
    This reverts commit eb3be5e7567d75495a02823b94fd70b4d5b22380.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -145,34 +145,4 @@ const logoutUser = async (req, res) => {
     }
 }
 
-const getUser = async(req, res) => {
-    const userId = req.user.id
-    if(!userId) {
-        return res
-        .status(401)
-        .json({
-            success:false,
-           message:"Unauthorized access"
-        })
-    }
-    const user = await User.findById(userId);
-    if(!user) {
-        return res
-            .status(401)
-            .json({
-                success: false,
-                message: "Unauthorized access"
-            })
-    }
-    return res
-    .status(200)
-    .json({
-        success:true,
-        user: {
-            id:userId,
-            name:user.name
-        }
-    })
-}
-
-export {registerUser, loginUser, logoutUser, getUser}
+export {registerUser, loginUser, logoutUser}
